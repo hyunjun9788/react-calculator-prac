d389cbe58ad7378a1c719a1d723d21095ff82c74
hyunjun9788 <namhyunjun9788@gmail.com>
2023-08-15 15:05:21 +0900

    chore: eslint 수정

--- src/FirstRow.js
+++ src/FirstRow.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 const FirstRow = () => {
-    return (
-
-            <div className='fitst-row'>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-            </div>
-
-    );
+  return (
+    <div className="fitst-row">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+    </div>
+  );
 };
 
 export default FirstRow;
